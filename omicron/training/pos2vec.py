02d08181f9567ba08484eddbeed73b8920d4d8e7
Tobias-Thomas <tobias.thomas@stud.tu-darmstadt.de>
2020-04-22 18:03:54 +0200

    switch back to preload dataset

--- omicron/training/pos2vec.py
+++ omicron/training/pos2vec.py
@@ -8,7 +8,9 @@ import numpy as np
 import os
 import random
 import math
-
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
 
 def _init_autoencoder_model(layers=[773, 600, 400, 200, 100]):
     """ Inits the auto_encoder model with the given layers. All layers are currently dense layers.
@@ -38,8 +40,8 @@ def _init_autoencoder_model(layers=[773, 600, 400, 200, 100]):
     return model
 
 
-def train_pos2vec_model(parsed_path_white, parsed_path_black, batch_size, epoch_size=2e6,
-                        num_epochs=200, white_share=0.5, layers=[773, 600, 400, 200, 100]):
+def train_pos2vec_model(parsed_path_white, parsed_path_black, batch_size, num_epochs=200,
+                        epoch_size=int(2e6), white_share=0.5, layers=[773, 600, 400, 200, 100]):
     """ Trains the pos2vec model, by selecting random inputs from the two given paths every batch.
 
     Args:
@@ -48,8 +50,7 @@ def train_pos2vec_model(parsed_path_white, parsed_path_black, batch_size, epoch_
         parsed_path_black (str): The path to the directory containing positions black wins.
             Currently only supports file of the type created with omicron.util.db_preprocess
         batch_size (int): The batch size to train on
-        epoch_size (int, optional): The number of samples in each epoch. This should be a multiple
-            of the batch_size.
+        epoch_size (int, optional): The number of samples in each epoch
         num_epochs (int, optional): The number of epochs to train on
         white_share (float, optional): The fraction of training data that comes from positions
             that are positive for white. This number can any be between 0 and 1
@@ -60,10 +61,25 @@ def train_pos2vec_model(parsed_path_white, parsed_path_black, batch_size, epoch_
         tensorflow.keras.models.Model: The trainined model
         tensorflow.keras.callbacks.History: The history of the training
     """
+    def load_training_samples():
+        white_samples = int(epoch_size * white_share)
+        white_games = os.listdir(parsed_path_white)
+        black_games = os.listdir(parsed_path_black)
+        examples = np.zeros((epoch_size,773))
+        for i in range(white_samples):
+            random_game = random.choice(white_games)
+            random_game = load_matrix(parsed_path_white+random_game)
+            examples[i] = random_game[random.randint(0,min(9, random_game.shape[0]-1))]
+        for i in range(white_samples,epoch_size):
+            random_game = random.choice(black_games)
+            random_game = load_matrix(parsed_path_black+random_game)
+            examples[i] = random_game[random.randint(0,min(9, random_game.shape[0]-1))]
+        return (examples, examples)
+
+    samples, labels = load_training_samples()
     model = _init_autoencoder_model(layers)
-    seq = Pos2VecSequence(parsed_path_white, parsed_path_black, white_share, epoch_size, batch_size)
     with tf.device('/GPU:0'):
-        history = model.fit(seq, epochs=num_epochs, verbose=1, workers=4, use_multiprocessing=True)
+        history = model.fit(samples, labels, epochs=num_epochs, batch_size=batch_size, verbose=1)
     return model, history
 
 
@@ -81,33 +97,4 @@ def save_encoder(model, save_name, save_dir, enc_layers=4):
     """
     encoded = model.get_layer('encode%d'%(enc_layers-1)).output
     enc_model = Model(inputs=model.input, outputs=encoded)
-    enc_model.save_weights(weight_dir+save_name)
-
-
-class Pos2VecSequence(Sequence):
-    def __init__(self, parsed_white, parsed_black, white_share, epoch_size, batch_size):
-        self.epoch_size = epoch_size
-        self.batch_size = batch_size
-        self.parsed_white = parsed_white
-        self.parsed_black = parsed_black
-        self.white_share = white_share
-
-    def __len__(self):
-        return math.ceil(self.epoch_size / self.batch_size)
-
-    def __getitem__(self, idx):
-        random_inp = np.random.randint(0, 1, (self.batch_size,773), dtype=np.bool)
-        return (random_inp, random_inp)
-        batch_examples = np.zeros((self.batch_size,773))
-        white_samples = int(self.batch_size * self.white_share)
-        games = os.listdir(self.parsed_white)
-        random_white_games = random.sample(games, white_samples)
-        for g in random_white_games:
-            random_game = load_matrix(self.parsed_white+g)
-            batch_examples[i] = random_game[random.randint(0,9)]
-        games = os.listdir(self.parsed_black)
-        random_black_games = random.sample(games, self.batch_size-white_samples)
-        for g in random_black_games:
-            random_game = load_matrix(self.parsed_black+g)
-            batch_examples[i] = random_game[random.randint(0,9)]
-        return (batch_examples, batch_examples)
+    enc_model.save_weights(save_dir+save_name)
